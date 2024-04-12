1ad39b63c58667fdabd87202e0ab7e8acb66e841
5Jamea07 <5jamea07@solent.ac.uk>
2024-04-12 01:49:22 +0100

    Defaulted Home Page

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+import withSerwistInit from '@serwist/next';
+      
+const withSerwist = withSerwistInit({
+    swSrc: "app/sw.ts",
+    swDest: "public/sw.js",
+    disable: process.env.NODE_ENV === 'development',
+});
+         
+export default withSerwist({
+    // Your Next.js config
+});
